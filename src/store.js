0ed7beb6d3284041ab8d2548598bd76bc5afa3d8
subrata85 <subratabiswas@digitalaptech.co.in>
2020-05-01 13:42:42 +0530

    login is wip

---
+++ src/store.js
@@ -0,0 +1,10 @@
+// import { createStore, applyMiddleware } from "redux";
+// import { composeWithDevTools } from "redux-devtools-extension";
+// import Promise from "redux-promise-middleware";
+// import thunk from "redux-thunk";
+
+// import rootReducers from "../src/redux/reducer/index";
+
+// const middleware = composeWithDevTools(applyMiddleware(Promise(), thunk));
+
+// export default createStore(rootReducers, middleware);
